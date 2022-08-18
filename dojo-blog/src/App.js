052a02ea61f1f1fb06d2b54779df3edb94160137
forks007 <you@example.com>
2022-08-19 03:21:30 +0600

    fourth tutorial

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -2,10 +2,20 @@
 import './App.css';
 
 function App() {
+  const title = "welcome to the new blog"
+  const likes = "50"
+  //  const person = {name:'yoshi', age:'50', id:'1'}
+  const links = "http://www.google.com";
+
+
+
   return (
     <div className="App">
       <div className="cotent">
-        <h1>hello world</h1>
+        <h1>{title}</h1>
+        <p>liked {likes} times</p>
+        <p>[1,2,2,5]</p>
+        <a href={links}>Google site</a>
       </div>
     </div>
   );
