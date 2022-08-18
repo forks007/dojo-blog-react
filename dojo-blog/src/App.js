48ce245d86e1447a6ca0af1e5b3c501d94be6645
forks007 <you@example.com>
2022-08-19 03:36:42 +0600

    fifth tutorial

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,21 +1,21 @@
 
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = "welcome to the new blog"
-  const likes = "50"
+  // const title = "welcome to the new blog"
+  // const likes = "50"
   //  const person = {name:'yoshi', age:'50', id:'1'}
-  const links = "http://www.google.com";
+  // const links = "http://www.google.com";
 
 
 
   return (
     <div className="App">
+      <Navbar/>
       <div className="cotent">
-        <h1>{title}</h1>
-        <p>liked {likes} times</p>
-        <p>[1,2,2,5]</p>
-        <a href={links}>Google site</a>
+        <Home/>
       </div>
     </div>
   );
