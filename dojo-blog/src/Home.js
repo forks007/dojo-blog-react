075cb824e2e6121d5c2841e426b13fded1cc11f3
forks007 <you@example.com>
2022-08-23 15:07:36 +0600

    useEffect Tutorial

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState , useEffect } from "react";
 import BlogList from "./BlogList";
 
 
@@ -11,17 +11,25 @@ const Home = () => {
 
     ])
 
+    const [name,setName] = useState("Mario")
+
     const handleDelete = (id) => {
         const newBlog = blogs.filter(blog => blog.id !== id)
         setBlog(newBlog);
     }
     
+    useEffect(()=>{
+        console.log("useeffect ran")
+    },[blogs])
     
     return ( 
         <div className="home">
             
             <BlogList blogs = {blogs} title="All the Blogs" handleDelete={handleDelete}/>
             <BlogList blogs = {blogs.filter((blog) => blog.author === "mario")} title="Marios Blogs" handleDelete={handleDelete}/>
+
+            <p>The name is {name}</p>
+            <button onClick={()=> setName("luigi")}>Click</button>
         </div>
      );
 }
