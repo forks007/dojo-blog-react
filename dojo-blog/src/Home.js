def3457332e0de1a4358e4f62172bc40acc272ac
forks007 <you@example.com>
2022-08-24 01:17:21 +0600

    custom hook

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,33 +1,18 @@
-import { useState , useEffect } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
-
-
+import useFetch from "./useFetch";
 
 const Home = () => {
-    const [blogs , setBlog] = useState(null)
-    const [isPending, setPending] = useState(true)
+  
+    const {data:blogs , isPending, error} = useFetch('http://localhost:8000/blogs')
 
-
-
-    
-    useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-          .then(res => {
-            return res.json();
-          })
-          .then(data => {
-            setBlog(data);
-            setPending(false);
-          })
-      }, [])
-    
-    return ( 
-        <div className="home">
-            {isPending && <div>Loading...</div> }
-            {blogs && <BlogList blogs = {blogs} title="All the Blogs"/>}
-            
-        </div>
-     );
+  return (
+    <div className="home">
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
+    </div>
+  );
 }
  
 export default Home;
